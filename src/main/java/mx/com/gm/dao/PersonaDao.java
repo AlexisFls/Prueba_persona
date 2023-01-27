9acb6c572e122535d48e22798bdbb78acb7f53b9
AlexisFls <AlexisFls>
2023-01-27 17:38:35 -0600

    shit

--- src/main/java/mx/com/gm/dao/PersonaDao.java
+++ src/main/java/mx/com/gm/dao/PersonaDao.java
@@ -3,7 +3,7 @@ package mx.com.gm.dao;
 
 import mx.com.gm.domain.Persona;
 import org.springframework.data.repository.CrudRepository;
-//camara mi perro
+//camara mi perrodmmdmd
 public interface PersonaDao extends CrudRepository<Persona, Long>{
     
 }
